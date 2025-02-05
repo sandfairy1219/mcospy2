ee22992dd4e5c68112763f5048273ab4963c7455
MswTester <mswtester10417@gmail.com>
2025-02-06 07:09:00 +0900

    mid save

--- src/scripts/test.ts
+++ src/scripts/test.ts
@@ -3,22 +3,29 @@ Java.perform(() => {
     javaSystem["exit"].implementation = function () {
         console.log("java.lang.System.exit");
     };
+    javaSystem["gc"].implementation = function () {
+        console.log("java.lang.System.gc");
+    };
+    let Runtime = Java.use("java.lang.Runtime");
+    Runtime["exit"].implementation = function () {
+        console.log("java.lang.Runtime.exit");
+    };
     let Cocos2dxActivity = Java.use("org.cocos2dx.lib.Cocos2dxActivity");
     Cocos2dxActivity["getCookie"].implementation = function (str:string) {
         let result = this["getCookie"](str);
         console.log("Cocos2dxActivity.getCookie", result);
         return result;
     };
-    // let XigncodeClientSystem = Java.use("com.wellbia.xigncode.XigncodeClientSystem");
+    let XigncodeClientSystem = Java.use("com.wellbia.xigncode.XigncodeClientSystem");
     // XigncodeClientSystem["initialize"].implementation = function (activity:any,str:string,str2:string,str3:string,callback:(...args:any[]) => void) {
     //     console.log("XigncodeClientSystem.initialize", activity, str, str2, str3);
     //     return 0;
     // };
-    // XigncodeClientSystem["getCookie2"].implementation = function (str:string) {
-    //     let result = this["getCookie2"](str);
-    //     console.log("XigncodeClientSystem.getCookie2", result);
-    //     return result;
-    // }
+    XigncodeClientSystem["getCookie2"].implementation = function (str:string) {
+        let result = this["getCookie2"](str);
+        console.log("XigncodeClientSystem.getCookie2", result);
+        return result;
+    }
     const filter = "Cocos2dxActivity";
     Java.enumerateLoadedClassesSync().forEach(function (className) {
         if (className.indexOf(filter) >= 0) {
